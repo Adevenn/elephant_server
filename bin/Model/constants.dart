5aad91eab7e9da18ad5ee2501b7f12b26aa2feae
Adrien Evennou <adrien.ev@hotmail.com>
2022-09-23 19:49:48 +0200

    rename app

--- bin/Model/constants.dart
+++ bin/Model/constants.dart
@@ -5,11 +5,11 @@ class Constants{
 
   //Auth DB
   static const authName = 'authentication';
-  static const authIP = '192.168.0.100';
+  static const authIP = '192.168.0.60';
   static const authPort = 7412;
 
   //Data DB
   static const dataName = 'my_netia';
-  static const dataIP = '192.168.0.100';
+  static const dataIP = '192.168.0.60';
   static const dataPort = 7412;
 }
